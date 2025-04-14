65bc0048f1f9feff4d295d0e29764fc2e4e3b20e
XiaoGanCN <76635216+XiaoGanCN@users.noreply.github.com>
2025-04-14 23:35:38 +0800

    Update C.java

--- src/com/skytechbytes/playerstatuebuilder/builder/C.java
+++ src/com/skytechbytes/playerstatuebuilder/builder/C.java
@@ -7,5 +7,6 @@ public interface C {
 	public static final int CONCRETE = 3;
 	public static final int GLASS = 4;
 	public static final int GRAY = 5;
+	public static final int ALL = 6;
 	public static final String[] PARTS = {"right_leg","left_leg","body","right_arm","left_arm","head"};
 }
